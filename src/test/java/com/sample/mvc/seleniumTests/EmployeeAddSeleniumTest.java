d105f07d71629bcdb4bb6e6cc7cca1e2aff65cc5
unknown <tanvig@Tanvig-w8.cybage.com>
2015-05-29 11:22:36 +0530

    final

--- src/test/java/com/sample/mvc/seleniumTests/EmployeeAddSeleniumTest.java
+++ src/test/java/com/sample/mvc/seleniumTests/EmployeeAddSeleniumTest.java
@@ -1,63 +1,52 @@
 package com.sample.mvc.seleniumTests;
 
-import static org.junit.Assert.*;
-
+import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class EmployeeAddSeleniumTest {
-	private WebDriver driver;
-	private String baseUrl;
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
 
-	@Before
-	public void setUp() throws Exception {
-		driver = new FirefoxDriver();
-		baseUrl = "http://172.27.59.109:8088/EmployeeApplicationSprint4-1.0-SNAPSHOT/jsp/add.jsp";
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	}
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "http://172.27.59.131:4040/EmployeeApplicationSprint4-1.0-SNAPSHOT/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
 
-	@Test
-	public void testCheck() throws Exception {
-	//driver.get(baseUrl + "");
-	    
-		//driver.findElement(By.name("code")).clear();
-	    //driver.findElement(By.name("code")).sendKeys("423");
-	    //driver.findElement(By.name("name")).clear();
-	    //driver.findElement(By.name("name")).sendKeys("Rahul");
-	    //driver.findElement(By.name("city")).clear();
-	    //driver.findElement(By.name("city")).sendKeys("Pune");
-	    //driver.findElement(By.id("SubmitEmployee")).click();
-		
-		
-		driver.get("http://localhost:8088/EmployeeApplicationSprint4-1.0-SNAPSHOT/home.html");
-	   // assertEquals("Employee Application", driver.getTitle());
-	    driver.findElement(By.id("AddEmployeeLink")).click();
-	    //assertEquals("Employee Application", driver.getTitle());
-	    driver.findElement(By.name("code")).clear();
-	    driver.findElement(By.name("code")).sendKeys("00");
-	    driver.findElement(By.name("name")).clear();
-	    driver.findElement(By.name("name")).sendKeys("ritu");
-	    driver.findElement(By.name("city")).clear();
-	    driver.findElement(By.name("city")).sendKeys("pune");
-	    driver.findElement(By.id("SubmitEmployee")).click();
-	    //assertEquals("Employee List", driver.getTitle());
-		
+  @Test
+  public void EmployeeAddSelenium() throws Exception {
+    driver.get("http://localhost:4040/EmployeeApplicationSprint4-1.0-SNAPSHOT/");
+    assertEquals("Employee Application", driver.getTitle());
+    driver.findElement(By.id("AddEmployeeLink")).click();
+    assertEquals("Employee Application", driver.getTitle());
+    driver.findElement(By.name("code")).clear();
+ 
+    driver.findElement(By.name("code")).sendKeys("121");
+    driver.findElement(By.name("name")).clear();
+    driver.findElement(By.name("name")).sendKeys("ritu");
+    driver.findElement(By.name("city")).clear();
+    driver.findElement(By.name("city")).sendKeys("kalyan");
+    driver.findElement(By.id("SubmitEmployee")).click();
+    assertEquals("Employee List", driver.getTitle());
+  }
 
-	}
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    
+    }
+  }
 
-	@After
-	public void tearDown() throws Exception {
-		driver.quit();
-
-	}
-
-}
+ 
 
 
 
--- src/test/java/com/sample/mvc/seleniumTests/EmployeeDeleteSeleniumTest.java
+++ src/test/java/com/sample/mvc/seleniumTests/EmployeeDeleteSeleniumTest.java
@@ -1,42 +1,42 @@
 package com.sample.mvc.seleniumTests;
 
-import static org.junit.Assert.*;
-
+import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class EmployeeDeleteSeleniumTest {
-  private WebDriver driver;
-  private String baseUrl;
-  @Before
-  public void setUp() throws Exception {
-    driver = new FirefoxDriver();
-    baseUrl = "http://172.27.59.109:8088/EmployeeApplicationSprint4-1.0-SNAPSHOT/list";
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
+	 private WebDriver driver;
+	  private String baseUrl;
+	  private boolean acceptNextAlert = true;
+	  private StringBuffer verificationErrors = new StringBuffer();
 
-  @Test
-  public void testEdit() throws Exception {
-	  driver.get("http://localhost:8088/EmployeeApplicationSprint4-1.0-SNAPSHOT/list");
-	    assertEquals("Employee List", driver.getTitle());
-	    driver.findElement(By.linkText("delete")).click();
-	    assertEquals("Employee List", driver.getTitle());
-	    driver.findElement(By.linkText("Back to Main")).click();
+	  @Before
+	  public void setUp() throws Exception {
+	    driver = new FirefoxDriver();
+	    baseUrl = "http://172.27.59.131:4040/EmployeeApplicationSprint4-1.0-SNAPSHOT/";
+	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	  }
+
+	  @Test
+	  public void EmployeeDeleteSelenium() throws Exception {
+	    driver.get("http://localhost:4040/EmployeeApplicationSprint4-1.0-SNAPSHOT/");
 	    assertEquals("Employee Application", driver.getTitle());
+	    driver.findElement(By.linkText("List Employees")).click();
+	    assertEquals("Employee List", driver.getTitle());
+	    driver.findElement(By.xpath("(//a[contains(text(),'delete')])[3]")).click();
+	    assertEquals("Employee List", driver.getTitle());
+	  }
+
+	  @After
+	  public void tearDown() throws Exception {
+	    driver.quit();
+    
    
   }
 
-  @After
-  public void tearDown() throws Exception {
-    driver.quit();
-   
-  }
-
- 
 }
